19ef4bb22a09450aee8ab0792c74e598a80836cd
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-03-05 12:01:15 +0530

    refactor Skills component; improve code formatting and enhance hover effects for better user experience

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,26 +5,25 @@ import { VscVscode } from "react-icons/vsc";
 import { IoLogoFirebase } from "react-icons/io5";
 
 const skills = [
-  {name: 'HTML', icon: SiHtml5, color: '#e34f26'},
-  {name: 'CSS', icon: SiCss3, color: '#1572b6'},
-  {name: 'JavaScript', icon: SiJavascript, color: '#f7df1e'},
+  { name: 'HTML', icon: SiHtml5, color: '#e34f26' },
+  { name: 'CSS', icon: SiCss3, color: '#1572b6' },
+  { name: 'JavaScript', icon: SiJavascript, color: '#f7df1e' },
   { name: 'TypeScript', icon: SiTypescript, color: '#3178c6' },
   { name: 'React', icon: FaReact, color: '#61dafb' },
   { name: 'Tailwind CSS', icon: SiTailwindcss, color: '#38bdf8' },
   { name: 'Node.js', icon: FaNodeJs, color: '#68a063' },
-  { name: 'Express.js', icon: SiExpress , color: '' },
+  { name: 'Express.js', icon: SiExpress, color: '' },
   { name: 'MongoDB', icon: SiMongodb, color: '#47a248' },
   { name: 'Next.js', icon: SiNextdotjs, color: '' },
   { name: 'Java', icon: FaJava, color: '#f7b731' },
   { name: 'Python', icon: FaPython, color: '#306998' },
   { name: 'C++', icon: SiCplusplus, color: '#00599c' },
   { name: 'VS Code', icon: VscVscode, color: '#007acc' },
-  {name: 'Firebase', icon: IoLogoFirebase , color: '#ffca28'},
+  { name: 'Firebase', icon: IoLogoFirebase, color: '#ffca28' },
   { name: 'Vite', icon: SiVite, color: '#646cff' },
   { name: 'Git', icon: SiGit, color: '#f34f29' },
   { name: 'GitHub', icon: SiGithub, color: '' },
 ];
-
 
 export function Skills() {
   return (
@@ -38,7 +37,7 @@ export function Skills() {
               {skills.map((tech) => (
                 <div
                   key={tech.name}
-                  className="flex flex-col items-center p-2 sm:p-4 sm:border border-gray-300 dark:border-gray-600 rounded-lg  shadow-md hover:shadow-xl hover:scale-105 transform transition-transform duration-300"
+                  className="flex flex-col items-center p-2 sm:p-4 sm:border border-gray-300 dark:border-gray-600 rounded-lg shadow-md hover:shadow-xl hover:bg-gray-200 dark:hover:bg-gray-800 transition-transform duration-300 transform hover:scale-110"
                 >
                   <tech.icon size={40} color={tech.color} />
                   <span className="text-sm font-medium text-gray-800 dark:text-gray-200 mt-2">{tech.name}</span>
